52e3025d9ba171fa59255f64c257fc20a91a4d28
viru <ezequielprovision@hotmail.com>
2020-02-04 20:48:22 -0300

    incorpore la lista e importe random

--- ahorcado.py
+++ ahorcado.py
@@ -1,1 +1,9 @@
-print("welcome")
+import random
+
+print("Bienvenides al AhoArcade! \n Comencemos! \n")
+
+lista_palabras = ["pablo", "viru", "otorrinolaringologo", "roquefort", "helicoptero", "cualquiera"]
+
+quest = random.choice(lista_palabras)
+
+print(quest)
