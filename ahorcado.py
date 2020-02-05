d4d2e9ac5de808201274682f60bd77733916d262
viru <ezequielprovision@hotmail.com>
2020-02-04 21:16:47 -0300

    funciona pero solo ubica una letra, si esta mas de una vez no la registra

--- ahorcado.py
+++ ahorcado.py
@@ -1,5 +1,5 @@
 import random
-
+intentos = 7
 print("Bienvenides al AhoArcade! \n Comencemos! \n")
 
 lista_palabras = ["pablo", "viru", "otorrinolaringologo", "roquefort", "helicoptero", "cualquiera"]
@@ -10,3 +10,21 @@ muestra = quest[0] + ("." * (len(quest) - 2)) + quest[-1]
 
 print(quest)
 print(muestra)
+
+
+while intentos > 0:
+
+    letra = input("ingresa una letra\n")
+    if letra in quest:
+        print("Está ubicada en {}".format(quest.index(letra) + 1))
+        position = quest.index(letra)
+        muestra = list(muestra)
+        muestra[position] = letra
+        muestra = "".join(muestra)
+        print(muestra)
+    
+    else:
+        print("nooooo")
+
+    intentos -= 1
+    print("Quedan {} !!".format(intentos))
