1c9b1892e25af276ff8d53f2b7409a26f70c2870
viru <ezequielprovision@hotmail.com>
2020-02-04 21:48:30 -0300

    se lo mejoro con funciones

--- ahorcado.py
+++ ahorcado.py
@@ -1,12 +1,28 @@
 import random
+###############################################################
+def busca_indices(letra, quest):
+    y = []
+    for index, x in enumerate(quest):  # genera en index el indice, y en x el objeto dentro de quest
+        if x == letra:
+            y.append(index)
+
+    return y
+
+def match_viewer(indices, quest, muestra, letra):
+    position = quest.index(letra)
+    muestra = list(muestra)
+    muestra[position] = letra
+    muestra = "".join(muestra)
+    return muestra
+################################################################
 intentos = 7
-print("Bienvenides al AhoArcade! \n Comencemos! \n")
 
 lista_palabras = ["pablo", "viru", "otorrinolaringologo", "roquefort", "helicoptero", "cualquiera"]
 
 quest = random.choice(lista_palabras)
-
 muestra = quest[0] + ("." * (len(quest) - 2)) + quest[-1]
+
+print("Bienvenides al AhoArcade! \n Comencemos! \n")
 
 print(quest)
 print(muestra)
@@ -15,16 +31,9 @@ print(muestra)
 while intentos > 0:
 
     letra = input("ingresa una letra\n")
-    if letra in quest:
-        print("Está ubicada en {}".format(quest.index(letra) + 1))
-        position = quest.index(letra)
-        muestra = list(muestra)
-        muestra[position] = letra
-        muestra = "".join(muestra)
-        print(muestra)
-    
-    else:
-        print("nooooo")
+    indices = busca_indices(letra, quest)
+    muestra = match_viewer(indices, quest, muestra, letra)
+    print(muestra)
 
     intentos -= 1
     print("Quedan {} !!".format(intentos))
