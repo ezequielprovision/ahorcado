2de246925d9e6283701591abe94cfd9bef82c761
viru <ezequielprovision@hotmail.com>
2020-03-03 20:27:00 -0300

    ahoracado.py ver 1.0

--- ahorcado.py
+++ ahorcado.py
@@ -1,39 +1,205 @@
 import random
+
 ###############################################################
-def busca_indices(letra, quest):
-    y = []
+def busca_indices(letra, quest, muestra):
+    muestra = list(muestra)
     for index, x in enumerate(quest):  # genera en index el indice, y en x el objeto dentro de quest
         if x == letra:
-            y.append(index)
-
-    return y
-
-def match_viewer(indices, quest, muestra, letra):
-    position = quest.index(letra)
-    muestra = list(muestra)
-    muestra[position] = letra
-    muestra = "".join(muestra)
+            muestra[index] = letra
     return muestra
 ################################################################
-intentos = 7
 
-lista_palabras = ["pablo", "viru", "otorrinolaringologo", "roquefort", "helicoptero", "cualquiera"]
+def letras_utilizadas(letra, lista):
+    
+    if letra in lista:
+        return True
+
+################################################################
+
+def convierte_string(muestra):
+    """ 
+    result = ""
+    aux = 0
+    for x in muestra:
+        if (aux == 0) or aux == (len(muestra) -1):
+            x = x.upper()
+        result += x
+        aux += 1
+    return result
+    """
+    muestra = list(muestra)
+    muestra[0] = muestra[0].upper()
+    muestra[-1] = muestra[-1].upper()
+    return "".join(muestra)
+###########################################################
+
+def grafico(intentos, muestra):
+    dibujo = ""
+    if intentos == 7:
+        dibujo = """
+    ---------------
+    |             |
+    |           
+    |           
+    |            
+    |            
+    |            
+    |
+      {}""".format(muestra)
+
+    if intentos == 6:
+        dibujo = """
+    ---------------
+    |             |
+    |           (¬.¬)
+    |           
+    |            
+    |            
+    |            
+    |
+      {}""".format(muestra)
+    elif intentos == 5:
+        dibujo = """
+    ---------------
+    |             |
+    |           (¬.¬)
+    |            /
+    |             
+    |           
+    |
+    |
+      {}""".format(muestra)
+
+    elif intentos == 4:
+        dibujo = """
+    ---------------
+    |             |
+    |           (¬.¬)
+    |            / \\
+    |             
+    |           
+    |
+    |
+      {}""".format(muestra)
+ 
+    elif intentos == 3:
+        dibujo = """
+    ---------------
+    |             |
+    |           (¬.¬)
+    |            / \\
+    |             |
+    |           
+    |
+    |
+      {}""".format(muestra)
+
+    elif intentos == 2:
+        dibujo = """
+    ---------------
+    |             |
+    |           (¬.¬)
+    |            / \\
+    |             |
+    |            /
+    |
+    |
+      {}""".format(muestra)
+    elif intentos == 1:
+        dibujo = """
+    ---------------
+    |             |
+    |           (¬.¬)
+    |            / \\
+    |             |
+    |            / \\
+    |
+    |
+      {}""".format(muestra)
+    elif intentos == 0:
+        dibujo = """
+    ---------------
+    |             |
+    |           (X.X) AAAAAAAAHHHHHHGGGG!
+    |           ======
+    |            / \\
+    |             |
+    |            / \\
+    |
+      {}""".format(muestra)
+
+    return dibujo
+
+###########################################################################
+
+lista_palabras = ["pablo",
+                 "anteojos",
+                 "abecedario",
+                 "virulana",
+                 "otorrinolaringologia",
+                 "roquefort",
+                 "helicoptero",
+                 "cualquiera",
+                 "etcetera",
+                 "parabrisas",
+                 "saturno",
+                 "mariposa",
+                 "serpentario",
+                 "departamento",
+                 "libertad",
+                 "ahorcado",
+                 "cartografia",
+                 "python3",
+                 "sombrilla",
+                 "manguera",
+                 "canguro",
+                 "legumbres",
+                 "marcadores",
+                 "simpatico",
+                 "cutis",
+                 "mañanero",
+                 "cargador"]
 
 quest = random.choice(lista_palabras)
+intentos = 7
+
+
 muestra = quest[0] + ("." * (len(quest) - 2)) + quest[-1]
 
-print("Bienvenides al AhoArcade! \n Comencemos! \n")
+muestra = busca_indices(muestra[0], quest, muestra) 
+muestra = busca_indices(muestra[-1], quest, muestra)
+print("Bienvenides al AhoArcade! \n*** Comencemos! ***\n")
 
-print(quest)
-print(muestra)
+print(convierte_string(muestra))
+lista_letras_usadas = [muestra[0], muestra[-1]]
 
 
 while intentos > 0:
+    aux = convierte_string(muestra)
+    letra = input("ingresa una letra, o arriesga con tu palabra definitiva\n")
+#    if len(letra) 
+    if letras_utilizadas(letra, lista_letras_usadas):
+        print("Esa letra ya está utilizada, pierdes un intento!")
+        intentos -= 1
+        muestra = convierte_string(muestra)
 
-    letra = input("ingresa una letra\n")
-    indices = busca_indices(letra, quest)
-    muestra = match_viewer(indices, quest, muestra, letra)
-    print(muestra)
+    else:
+        lista_letras_usadas.append(letra)
+        muestra = busca_indices(letra, quest, muestra)
+        muestra = convierte_string(muestra)
 
-    intentos -= 1
-    print("Quedan {} !!".format(intentos))
+        if aux == muestra:
+            print("No!")
+            intentos -= 1
+        else:
+            print("Muy bien!!")
+    
+    if not "." in muestra:
+        print("Sii!, Ganaste!!")
+        exit()
+    print(grafico(intentos, muestra))
+    
+    if intentos > 0:
+        print("Quedan {} !!".format(intentos))
+    else:
+        print("Que en paz descanse...")
