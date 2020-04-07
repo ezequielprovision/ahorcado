ed5d55293f252f32017f31f10ff7adead9c11bac
viru <ezequielprovision@hotmail.com>
2020-04-07 13:25:48 -0300

    solved adding word bug, functs to module, and menu_screen.option_2 loop (para salir ingrese 1)

--- ahorcado.py
+++ ahorcado.py
@@ -1,91 +1,11 @@
 import random
 import os
 import grafico
-#################    FUNCIONES DEL MENU   ####################
-def menu_option_2(battery_file):
-    with open(battery_file, "r") as f:
-        new_file = [] 
-        for line in f:
-            new_file.append(line.strip('\n'))   ### LEVANTA LAS PALABRAS DEL TXT
-
-    while True:               ### Voy a suponer q el usuario ingresa la palabra de la manera adecuada
-        new_word = input('Ingresa una palabra sin espacios ni caracteres raros\n')
-        if new_word == '1':   ### Sino tendria que hacer una funcion que compare todos los
-            break             ### caracteres a ver si son validos
-        elif not new_word in new_file:
-            new_file.append(new_word)
-            print('Agregado!, Ingresa otra palabra o \"1\" para salir')
-        else:
-            print('Esa palabra ya está en la lista')
-
-
-def menu_option_3(file):
-    with open(file, "r") as f:
-        for line in f:
-            print(line.strip("\n"))
-
-###############################################################
-def no_se_que_func_tiene_python_para_esto(y):
-    result = ""
-    for x in y:
-        result += x.upper() + " "
-    return result
-
-###############################################################
-def busca_indices(letra, quest, muestra):
-    muestra = list(muestra)
-    for index, x in enumerate(quest):  # genera en index el indice, y en x el objeto dentro de quest
-        if x == letra:
-            muestra[index] = letra
-    return muestra
-################################################################
-
-def letras_utilizadas(letra, lista):
-    if letra in lista:
-        return True
-
-################################################################
-
-def convierte_string(muestra):
-    """ 
-    result = ""
-    aux = 0
-    for x in muestra:
-        if (aux == 0) or aux == (len(muestra) -1):
-            x = x.upper()
-        result += x
-        aux += 1
-    return result
-    """
-    muestra = list(muestra)
-    muestra[0] = muestra[0].upper()
-    muestra[-1] = muestra[-1].upper()
-    return "".join(muestra)
-###########################################################
-
-def load_state(load_file):
-    result = {}
-    with open(load_file, 'r') as f:
-        for line in f:
-            line.strip("\n")
-            key, value = line.split(' ')
-            result[key] = int(value.strip('\n')) ## Saca salto de linea, y convierte en entero
-        return result
-
-##########################################################
-
-def save_state(score):
-    gamer_name = input("Ingresa tu nombre sin espacios en el medio \n")
-    score = str(score)
-    text_will_save = gamer_name + ' ' + score + '\n'
-    return text_will_save
+import menu_screen
+import engines
+import state
     
-###########################################################
 
-
-
-#########################  MENU INICIO  ####################################
-#battery_file = r"C:\Users\eromi\Documents\GitHub\proyectos\ahorcado\nombres_ahorc.txt"
 path = os.path.abspath(__file__)  # path entero de donde esta el archivo corriendo (en este caso ahorcado.py)
 path = os.path.dirname(path)  # directorio del archivo, solo resta sumarle el nombre del archivo
 battery_file = os.path.join(path, "nombres_ahorc.txt")  # usando os.path.join te aseguras 
@@ -105,10 +25,10 @@ while not start:
         start = True
 
     elif menu == "2":
-        menu_option_2(battery_file)
+        menu_screen.option_2(battery_file)
 
     elif menu == "3":
-        menu_option_3(battery_file)
+        menu_screen.option_3(battery_file)
     else:
         exit()
 
@@ -116,20 +36,18 @@ while not start:
 play = True
 score = 0
 _continue = None
-#loaded_file = r"C:\Users\eromi\Documents\GitHub\proyectos\ahorcado\saved_scores.txt"
 
-path = os.path.abspath(__file__)  # path entero de donde esta el archivo corriendo (en este caso ahorcado.py)
-path = os.path.dirname(path)  # directorio del archivo, solo resta sumarle el nombre del archivo
-loaded_file = os.path.join(path, "saved_scores.txt")  # usando os.path.join te aseguras 
-# que agregue las barras correctas dependiendo del sistema operativo, 
-# ya que en mac y linux es / en vez de \
+path = os.path.abspath(__file__)  
+path = os.path.dirname(path)  
+loaded_file = os.path.join(path, "saved_scores.txt")   
 
 
-if not os.path.exists(loaded_file):  ## No tengo idea si lo que estoy haciendo esta bien
-    with open(loaded_file, 'w') as f: ## Me lo pasaste como 'os.exists' y me tiro error
+
+if not os.path.exists(loaded_file):  
+    with open(loaded_file, 'w') as f: 
         f.write('')
 else:
-    table_score = load_state(loaded_file) # me va a devolver un dicc con los nombres y puntajes
+    table_score = state.load(loaded_file) # me va a devolver un dicc con los nombres y puntajes
     print('Puntajes anteriores: \n{}'.format(table_score))
 
 ############################################################################ 
@@ -161,18 +79,18 @@ while play:
 
     muestra = quest[0] + ("." * (len(quest) - 2)) + quest[-1]
 
-    muestra = busca_indices(muestra[0], quest, muestra) 
-    muestra = busca_indices(muestra[-1], quest, muestra)
+    muestra = engines.busca_indices(muestra[0], quest, muestra) 
+    muestra = engines.busca_indices(muestra[-1], quest, muestra)
     
     if _continue is None:  #de lo contrario va a mostrar el bienvenide cada vez q juegues
         print("Bienvenides al AhoArcade! \n*** Comencemos! ***\n")
 
-    print(convierte_string(muestra))
+    print(engines.convierte_string(muestra))
     lista_letras_usadas = [muestra[0], muestra[-1]]
 
 
     while intentos > 0:
-        aux = convierte_string(muestra)
+        aux = engines.convierte_string(muestra)
         letra = input("ingresa una letra, o arriesga con tu palabra definitiva\n")
         if len(letra) > 1:
             if letra == quest:
@@ -200,15 +118,15 @@ while play:
                     play = False
 
         else:   ### o sea si el len es = 1         
-            if letras_utilizadas(letra, lista_letras_usadas):
+            if engines.letras_utilizadas(letra, lista_letras_usadas):
                 print("Esa letra ya está utilizada, pierdes un intento!")
                 intentos -= 1
-                muestra = convierte_string(muestra)
+                muestra = engines.convierte_string(muestra)
 
             else:
                 lista_letras_usadas.append(letra)
-                muestra = busca_indices(letra, quest, muestra)
-                muestra = convierte_string(muestra)
+                muestra = engines.busca_indices(letra, quest, muestra)
+                muestra = engines.convierte_string(muestra)
 
                 if aux == muestra:
                     print("No!")
@@ -228,7 +146,7 @@ while play:
                     play = False
 
             else:  ### si todavia tiene '.' 
-                aux__ = no_se_que_func_tiene_python_para_esto(lista_letras_usadas)
+                aux__ = engines.string_used_words(lista_letras_usadas)
                 print("LETRAS YA USADAS: {}".format(aux__))
                 print(grafico.draw_sprite(intentos, muestra))
                 
@@ -243,7 +161,7 @@ while play:
                     if _continue == "n":  ## Al ingresar 'y' va a emplezar de nuevo
                         play = False
 
-to_save = save_state(score)
+to_save = state.save(score)
 
 with open(loaded_file, "r") as f:
     for line in f:
